5b7a15d450c17914dc8e3ad08ca9ba32cf0cc401
Laerthe Souza <laerte_souza.developer@outlook.com>
2021-11-05 00:16:16 -0300

    feat(users): adding route to list all users

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -1,8 +1,17 @@
 import { Request, Response } from 'express';
 
+import { ListUsersService } from '../services/ListUsersService';
 import { CreateUserService } from '../services/CreateUserService';
 
 export class UsersController {
+  async index(request: Request, response: Response) {
+    const listUsers = new ListUsersService();
+
+    const users = await listUsers.execute();
+
+    return response.status(200).json(users);
+  }
+
   async create(request: Request, response: Response) {
     const { name, email, password } = request.body;
 
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -6,6 +6,7 @@ const usersRoutes = Router();
 
 const usersController = new UsersController();
 
+usersRoutes.get('/', usersController.index);
 usersRoutes.post('/', usersController.create);
 
 export { usersRoutes };
