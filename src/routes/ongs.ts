8f068665b9a4ce739b41ff6e71e117b608169efa
Laerthe Souza <laerte_souza.developer@outlook.com>
2021-11-06 18:34:32 -0300

    feat(ongs): include method delete of the ong controller into routes

--- src/routes/ongs.ts
+++ src/routes/ongs.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
 import { OngsController } from '../controllers/OngsController';
+import { ensureAuthentication } from '../middlewares/ensureAuthentication';
 
 const ongsRoutes = Router();
 
 const ongsController = new OngsController();
 
 ongsRoutes.post('/', ongsController.create);
+ongsRoutes.delete('/', ensureAuthentication, ongsController.delete);
 
 export { ongsRoutes };
